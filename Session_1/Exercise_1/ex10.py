27b1944de042b8e6d57e273c59c7d5d5234c1140
yasaswini-devii <23071a0580@vnrvjiet.in>
2025-02-04 21:50:53 +0530

    generating random passwords using random function

---
+++ Session_1/Exercise_1/ex10.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print(generate_password(int(input('Enter length: ')))) 
